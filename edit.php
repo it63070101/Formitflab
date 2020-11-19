7e6f40efc35ecad03ff35ea232ef06cae5472d9a
it63070101 <73680308+it63070101@users.noreply.github.com>
2020-11-19 17:04:53 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -25,7 +25,7 @@
 	</style>
 </head>
 <body class="bg-light py-5">
-	<div class="container text-dark">
+	<div class="container text-success">
 		<div class="row">
 			<div class="col-12 col-lg-8 offset-lg-2">
 				<div class="card shadow">
